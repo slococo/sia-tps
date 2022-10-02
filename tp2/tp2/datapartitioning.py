43dba7abc3a41d0ca2f16f79bb08c0d436c04d5d
salvaCasta276 <salCas276@gmail.com>
2022-10-02 19:06:57 -0300

    Made changed on resizing of data arrays

--- tp2/tp2/datapartitioning.py
+++ tp2/tp2/datapartitioning.py
@@ -23,7 +23,7 @@ def holdout(dataset, training_probability, activation_function, eta, neurone_mat
     results = perceptron.predict(dataset[- int(len(dataset)*(1-training_probability)):, :-1])
 
     #TODO: mirar que la dimension este bien
-    expected = dataset[int(len(dataset)*(1-training_probability)):, -1:]
+    expected = np.squeeze(dataset[int(len(dataset)*(1-training_probability)):, -1:])
 
     return 0
 
@@ -32,18 +32,18 @@ def k_fold(dataset, k, activation_function, eta, neurone_matrix, error, max_iter
     dataset = np.array(dataset)
     partition_size = int(len(dataset)/k)
 
-    partitioned_dataset = dataset[:partition_size*k-len(dataset)].reshape(k, partition_size)
+    partitioned_dataset = dataset[:partition_size*k-len(dataset)].reshape(k, partition_size, 4)
 
     for i in range(k):
         perceptron = Perceptron(
             neurone_matrix, None, activation_function, activation_function, len(neurone_matrix) + 1, eta
         )
 
-        perceptron.train(dataset[i*partition_size:(i+1)*partition_size], error, max_iter, learning)
+        perceptron.train(dataset[Elimirar la row que no quiero].reshape(k*partition_size, 4), error, max_iter, learning)
 
         results = perceptron.predict(partitioned_dataset[i])
 
         # TODO: mirar que la dimension este bien
-        expected = partitioned_dataset[i, -1:]
+        expected = np.squeeze(partitioned_dataset[i, -1:])
 
     return 0
