71e151d3bd68fc8a8b22344f2d2d76d9357b1d85
ebellver <ebellver@itba.edu.ar>
2022-10-05 10:32:23 -0300

    make animation
    Co-authored-by: Santiago Lo Coco <slococo@itba.edu.ar>
    Co-authored-by: Juan Barmasch <jbarmasch@itba.edu.ar>

--- tp2/tp2/ej2/animation.py
+++ tp2/tp2/ej2/animation.py
@@ -1,54 +1,39 @@
-import gc
-import resource
-
 import matplotlib
 
-
 import matplotlib.pyplot as plt
-import pandas as pd
-
-from tp2 import utils
 from tp2.perceptron import Perceptron
 
 matplotlib.use("TkAgg")
 
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import PillowWriter
-# import gc
+
 
 def create_animation(data, historic):
-    fig = plt.figure(figsize=(14, 9))
+    fig = plt.figure(figsize=(6, 4))
     ax = plt.axes(projection="3d")
     print(data)
 
     def animate(i):
-        # if not i % 100:
-        #     gc.collect()
         ax.clear()
+
         ax.set_ylim([-2, 8])
         ax.set_xlim([-2, 8])
         ax.set_zlim([-2, 8])
 
         res = historic[i]
-
         for j in range(0, len(data)):
-            color = 0xFFFFFF * ((res[j][0] + 1) / 2)
-            ax.scatter(data[j][0], data[j][1], data[j][2], color="#{:06x}".format(round(color)))
+            val = 0xFF * ((res[j][0] + 1) / 2)
+            ax.scatter(data[j][0], data[j][1], data[j][2], color="#{:02x}{:02x}{:02x}".format(round(val), round(val), round(val)))
 
-    ani = FuncAnimation(fig, animate, frames=len(historic), interval=1000, repeat=False)
+    # ani = FuncAnimation(fig, animate, frames=len(historic), interval=1, repeat=False)
+    ani = FuncAnimation(fig, animate, frames=len(historic), interval=90, repeat=False)
+    # plt.show()
     plt.close()
-    ani.save("anim.gif", dpi=300, writer=PillowWriter(fps=1))
+    ani.save("anim.gif", writer='PillowWriter', fps=11)
     # plt.show()
 
 
 if __name__ == "__main__":
     perceptron = Perceptron.load()
-    # with open('/proc/meminfo', 'r') as mem:
-    #     free_memory = 0
-    #     for i in mem:
-    #         sline = i.split()
-    #     if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
-    #         free_memory += int(sline[1])
-    # soft, hard = resource.getrlimit(resource.RLIMIT_AS)
-    # resource.setrlimit(resource.RLIMIT_AS, (round(free_memory * 1024 * 0.9), hard))
     create_animation(perceptron.data, perceptron.historic)
